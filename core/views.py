a1755156864e3af0b83357993b8426484c415fcf
bruno antunes <68615491+BrunoAntunes21@users.noreply.github.com>
2022-06-24 22:37:55 -0300

    introdução da block content e views herdadas criação das paginas de erro e mudança do icone de erro

--- core/views.py
+++ core/views.py
@@ -4,5 +4,10 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name  ='index.html'
 
+class NowtView(TemplateView):
+  template_name = '404.html'
+class ServerErrorView(TemplateView):
+    template_name='500.html'
+
 
 # Create your views here.
