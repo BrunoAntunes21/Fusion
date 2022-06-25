57922535d587d019458a81da282b78b40e340f67
bruno antunes <68615491+BrunoAntunes21@users.noreply.github.com>
2022-06-24 22:37:55 -0300

    introdução da block content e views herdadas criação das paginas de erro e musança do icone de erro

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView,NowtView,ServerErrorView
 urlpatterns=[
-    path('',IndexView.as_view(),name='index')
+    path('',IndexView.as_view(),name='index'),
+    path('404/',NowtView.as_view(),name='404'),
+    path('500/',ServerErrorView.as_view(),name='500')
 ]
